1907bdc7ee0442d73919879f7cdf473877cce23a
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-12-05 19:35:57 +0100

    Created directories if they dont exist

--- Finger_analyzer.py
+++ Finger_analyzer.py
@@ -1,9 +1,9 @@
 import os
-import shutil
 import cv2
-import numpy as np
 
 def sort_images(source_dir, train_dir, test_dir):
+    create_directories_if_not_exist(train_dir, test_dir)
+
     image_files = []
     for file in os.listdir(source_dir):
         if file.endswith('.tif'):
@@ -11,11 +11,16 @@ def sort_images(source_dir, train_dir, test_dir):
 
     for index, file in enumerate(image_files, start=1):
         image_source = os.path.join(source_dir, file)
+        processed_image = process_image(image_source)
         if index % 8 == 0:
-            shutil.copy2(image_source, test_dir)
+            cv2.imwrite(os.path.join(test_dir, file), processed_image)
         else:
-            processed_image = process_image(image_source)
             cv2.imwrite(os.path.join(train_dir, file), processed_image)
+
+def create_directories_if_not_exist(*directories):
+    for directory in directories:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
 def process_image(image_path):
     gray_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -27,10 +32,17 @@ def main():
     training_directory = './TrainingSet'
     testing_directory = './TestSet'
 
+    create_directories_if_not_exist(source_directory, training_directory, testing_directory)
+
+    if not os.listdir(source_directory):
+        print("Source directory is empty! Fill it with fingerprint database.")
+        return
+
     if os.listdir(training_directory) or os.listdir(testing_directory):
-        print("Images already sorted and procesed.")
+        print("Images already sorted and processed.")
     else:
         sort_images(source_directory, training_directory, testing_directory)
+        print("Images have been sorted and processed.")
 
 if __name__ == "__main__":
     main()
